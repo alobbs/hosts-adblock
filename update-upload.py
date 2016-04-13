1cd2ed764e4a1829a31c9f7de849c781cd00e357
Alvaro Lopez Ortega <alvaro@gnu.org>
2016-04-13 19:33:40 +0200

    Adds date to commit message's title

--- update-upload.py
+++ update-upload.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import time
 
 # Move to the script directory
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
@@ -13,6 +14,7 @@ with os.popen("git diff hosts-adblock-alobbs") as f:
     has_update = len(f.read()) > 1
 
 if has_update:
+    now = time.strftime("%Y-%m-%d %Hh")
     os.system("git add hosts-adblock-alobbs")
-    os.system('git commit -m "Updates list of domains"')
+    os.system('git commit -m "Updates list of domains: %s"' % now)
     os.system("git push")
